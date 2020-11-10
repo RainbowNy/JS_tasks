c7e6b4de8400cd2e3bcf54aecbaf158963b018bc
RainNy <permetr35@gmail.com>
2020-11-10 19:27:24 +0300

    Add validation check of format of grid

--- js/task_3/task_3.js
+++ js/task_3/task_3.js
@@ -1,12 +1,30 @@
 function fromGridToArray(grid) {
     let arrayOfCells = [];
 
-    for(const rowOfCells of grid){
-        for(const cell of rowOfCells){
-            arrayOfCells.push(cell);
+    if(checkForValidGrid(grid)) {
+        for (const rowOfCells of grid) {
+            for (const cell of rowOfCells) {
+                arrayOfCells.push(cell);
+            }
+            arrayOfCells.push("split");
         }
-        arrayOfCells.push("split");
     }
 
     return arrayOfCells;
+}
+
+function checkForValidGrid(grid) {
+    if (typeof (grid) != "object" || grid.length === undefined || grid.length === 0) {
+        alert('Invalid format of grid');
+        return false;
+    }
+
+    for (let row of grid) {
+        if (typeof (row) != "object" || row.length === undefined || row.length === 0) {
+            alert(`Invalid format of row at ${grid.indexOf(row) + 1} position`);
+            return false;
+        }
+    }
+
+    return true;
 }
