3d7db3bbd79bda1b7a610cc7beb3ae5f5e842df1
RainNy <permetr35@gmail.com>
2020-11-19 21:19:26 +0300

    Fix. Changed implementation of build grid.

--- js/task_4/task_4.js
+++ js/task_4/task_4.js
@@ -1,16 +1,32 @@
 function fromArrayToGrid(arrayOfCells) {
     const grid = [];
     let rowOfCells = [];
+    let indexOfCurrentCellFromArray = 0;
+    let selectedCellFromArray;
+    let numberOfColumns;
+    let numberOfRows;
 
-    if(checkForValidArrayOfCells(arrayOfCells)) {
-        for (const cell of arrayOfCells) {
-            if (cell === 'split') {
-                grid.push(rowOfCells);
-                rowOfCells = [];
-                continue;
+    if (checkForValidArrayOfCells(arrayOfCells)) {
+        numberOfColumns = arrayOfCells.reduce((max, cell) => (cell.left + cell.width) > max ? cell.left + cell.width : max, 0);
+        numberOfRows = arrayOfCells.reduce((max, cell) => (cell.top + cell.height) > max ? cell.top + cell.height : max, 0);
+
+        for (let row = 0; row < numberOfRows; row++) {
+            for (let column = 0; column < numberOfColumns; column++) {
+                selectedCellFromArray = arrayOfCells[indexOfCurrentCellFromArray];
+
+                if ((selectedCellFromArray.left !== column) || (selectedCellFromArray.top !== row)) {
+                    rowOfCells.push(null);
+                    continue;
+                }
+
+                rowOfCells.push(selectedCellFromArray);
+                if (indexOfCurrentCellFromArray < arrayOfCells.length - 1) {
+                    indexOfCurrentCellFromArray++;
+                }
             }
 
-            rowOfCells.push(cell);
+            grid.push(rowOfCells);
+            rowOfCells = [];
         }
     }
 
@@ -18,7 +34,7 @@ function fromArrayToGrid(arrayOfCells) {
 }
 
 function checkForValidArrayOfCells(arrayOfCells) {
-    if(typeof(arrayOfCells) != 'object' || arrayOfCells.length === undefined || arrayOfCells.length === 0){
+    if (typeof (arrayOfCells) != 'object' || !arrayOfCells.length) {
         alert('Invalid format of array of cells');
         return false;
     }
